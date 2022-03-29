26579eb658ead2547f36d8d75c21c3bd8672209f
Leah Oswald <loswal1@live.spcollege.edu>
2022-03-29 16:32:37 -0400

    Client login and register: observables, angular template forms, async pipe, parent child communication vice versa

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +13,15 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
-  getUsers() {
-    this.http.get('https://localhost:5001/API/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 }
 
